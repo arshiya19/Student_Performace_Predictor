ae1b1e36a34d294221bc4ad7be78d86c19bbff60
Arshiya Naheed <72481315+arshiya19@users.noreply.github.com>
2024-08-08 20:53:40 -0700

    Data traansformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,7 +1,4 @@
 ''' code realted to data '''
-
-
-
 import os
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
@@ -11,6 +8,8 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -51,4 +50,7 @@ class DataIngestion:
         
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation = DataTranformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
